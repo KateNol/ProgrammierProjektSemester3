af071b5445b99a5759a5aeadb11a34b5fe1412b6
Jannik Rosendahl <jannik.rosendahl@stud.hs-hannover.de>
2022-10-18 22:04:39 +0200

    add utility print methods

--- src/network/internal/Util.java
+++ src/network/internal/Util.java
@@ -8,4 +8,13 @@ public final class Util {
 
     public static final int defaultPort = 13337;
 
+    private static final String logPrefix = "Network: ";
+
+    public static void log_stdio(String msg) {
+        System.out.println(logPrefix + msg);
+    }
+
+    public static void log_stderr(String msg) {
+        System.err.println(logPrefix + msg);
+    }
 }
