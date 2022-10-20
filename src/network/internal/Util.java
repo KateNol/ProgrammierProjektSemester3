23f0aeea63db18299eb5a390a60ab88c6ee09cf1
Jannik Rosendahl <jannik.rosendahl@me.com>
2022-10-20 11:42:51 +0200

    add util constants

--- src/network/internal/Util.java
+++ src/network/internal/Util.java
@@ -1,17 +1,12 @@
 package network.internal;
 
 public final class Util {
-    private Util() {
-    }
+    private Util() {}
 
     public static final String defaultAddress = "localhost";
-
     public static final int defaultPort = 13337;
 
-    private static final String logPrefix = "Network: ";
-
     public static final Character messageDelimiter = ';';
-
     public static String constructMessage(String command, String[] args) {
         StringBuilder builder = new StringBuilder();
 
@@ -26,11 +21,13 @@ public final class Util {
         return builder.toString();
     }
 
+    private static final String logPrefix = "Network: ";
     public static void log_stdio(String msg) {
         System.out.println(logPrefix + msg);
     }
-
     public static void log_stderr(String msg) {
         System.err.println(logPrefix + msg);
     }
+
+    public static final int implementedProtocolVersion = 1;
 }
