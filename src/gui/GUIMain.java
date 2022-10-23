f49e2434718c1ca4671ebf831ae2038d16bd7ad4
Jannik Rosendahl <jannik.rosendahl@stud.hs-hannover.de>
2022-10-23 21:17:31 +0200

    gui main class. start the application from this class

--- src/gui/GUIMain.java
+++ src/gui/GUIMain.java
@@ -1,5 +1,6 @@
 package gui;
 
+import gui.controllers.GUIPlayer;
 import internal.LocalEnemyMode;
 import internal.NetworkMode;
 import internal.PlayerMode;
@@ -11,7 +12,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import logic.Logic;
 import logic.Player;
+import network.debug.ConsolePlayer;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
@@ -45,11 +48,21 @@ public class GUIMain extends Application {
     public void changeSceneToGame(PlayerMode playerMode, NetworkMode networkMode, ServerMode serverMode, LocalEnemyMode localEnemyMode, String address, String port) throws IOException {
         root = FXMLLoader.load(getClass().getResource("fxml/game.fxml"));
         scene = new Scene(root);
-        stage.setTitle("Game");
+        stage.setTitle("GUIPlayer");
         stage.setScene(scene);
         stage.show();
 
+        /*
+        at this point we create the actual players and logic instances,
+        here we could switch over all the parameters and instantiate the correct players,
+        but for now we just use a gui player and a console player
+        note: if the enemy is a networkplayer, we don't need another logic instance for him (?)
+         */
+        Player player = GUIPlayer.getInstance();
+        Player enemy = new ConsolePlayer(network.NetworkMode.CLIENT);
 
+        Logic playerLogic = new Logic(player, enemy);
+        Logic enemyLogic = new Logic(enemy, player);
     }
 
     public static GUIMain getInstance() {
