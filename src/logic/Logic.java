a2507747b657a6cb0064577f4eb7f7b41a36e705
Jannik Rosendahl <jannik.rosendahl@stud.hs-hannover.de>
2022-10-23 21:18:43 +0200

    nearly empty logic class. holds reference to player and enemy, in the future this class can hold all game logic. it should communicate with the player (and the gui if present)

---
+++ src/logic/Logic.java
@@ -0,0 +1,11 @@
+package logic;
+
+public class Logic {
+    Player player;
+    Player enemy;
+
+    public Logic(Player player, Player enemy) {
+        this.player = player;
+        this.enemy = enemy;
+    }
+}
