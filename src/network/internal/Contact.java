783cd7eaeeda0052f6c1a72f8f4563064eda4c04
Jannik Rosendahl <jannik.rosendahl@stud.hs-hannover.de>
2022-10-27 19:46:47 +0200

    update to new protocol version. END, END_ACK, BYE, ERR not implemented yet

--- src/network/internal/Contact.java
+++ src/network/internal/Contact.java
@@ -1,6 +1,7 @@
 package network.internal;
 
 import network.NetworkMode;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,8 +11,8 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-import static network.internal.MessageMode.SEND;
 import static network.internal.MessageMode.RECEIVE;
+import static network.internal.MessageMode.SEND;
 import static network.internal.Util.*;
 
 /**
@@ -39,35 +40,39 @@ public final class Contact {
 
 
     Contact(Socket socket, NetworkMode networkMode, String username, int semester) throws IOException {
-        this.socket = socket;
         this.networkMode = networkMode;
         this.username = username;
         this.semester = semester;
 
-        if (this.socket != null) {
-            this.outWriter = new PrintWriter(this.socket.getOutputStream(), true);
-            this.inReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-        }
-
         // fallback as per spec
         this.protocolVersion = 1;
 
-        if (this.socket != null) {
-            new Thread(this::receiveLoop).start();
-            HELLO(SEND, this.username);
-            VERSION_SEND(SEND, implementedProtocolVersion);
-            SEMESTER_SEND(SEND, semester);
+        if (socket != null) {
+            setSocket(socket);
+            init_communication();
+        } else {
+            this.socket = null;
         }
-        System.out.println("Contact CTOR end");
 
+        System.out.println("Contact CTOR end");
     }
 
+
     Contact(Socket socket, NetworkMode networkMode) throws IOException {
-        this(socket, networkMode, "undefined", 1);
+        this(socket, networkMode, networkMode.name(), 1);
     }
 
     Contact(NetworkMode networkMode) throws IOException {
         this(null, networkMode);
+    }
+
+    private void init_communication() {
+        new Thread(this::receiveLoop).start();
+
+        // if hosted, send HELLO
+        if (networkMode == NetworkMode.SERVER) {
+            HELLO(SEND, implementedProtocolVersion, semester, username);
+        }
     }
 
     private void receiveLoop() {
@@ -77,7 +82,7 @@ public final class Contact {
                 if (input == null) {
                     throw new IOException("Network received null input, peer seems to have disconnected");
                 }
-                log_stdio("received '" + input + "'");
+                log_debug("received: '" + input + "'");
                 input = input.toUpperCase();
 
                 ArrayList<String> args = new ArrayList<>(List.of(input.split(String.valueOf(messageDelimiter))));
@@ -88,148 +93,271 @@ public final class Contact {
             }
         } catch (IOException e) {
             log_stderr(e.getMessage());
+        } catch (IndexOutOfBoundsException e) {
+            log_stderr(e.getMessage());
         }
     }
 
     /**
-     * evaluates received messages
+     * parses incoming messages and sends them to their according methods for handling
      */
-    private void eval(String command, ArrayList<String> args) {
-        //log_stdio("eval got {" + command + "} {" + args + "}");
-        if (command.equals("HELLO")) {
-            // HELLO;<USERNAME>;[VERSION]
-        } else if (command.equals("VERSION_SEND")) {
-            // VERSION_SEND;<VERSION>
-            int peerVersion = Integer.parseInt(args.get(0));
-            VERSION_SEND(RECEIVE, peerVersion);
-        } else if (command.equals("VERSION_ACK")) {
-            // VERSION_ACK;<VERSION>
-            int peerVersion = Integer.parseInt(args.get(0));
-            VERSION_ACK(RECEIVE, peerVersion);
-        } else if (command.equals("SEMESTER_SEND")) {
-            // SEMESTER_SEND;<SEMESTER>
-            int peerSemester = Integer.parseInt(args.get(0));
-            SEMESTER_SEND(RECEIVE, peerSemester);
-        } else if (command.equals("SEMESTER_ACK")) {
-            // SEMESTER_ACK;<SEMESTER>
-            int peerSemester = Integer.parseInt(args.get(0));
-            SEMESTER_ACK(RECEIVE, peerSemester);
-        }
-
-
-    }
-
-    private void HELLO(MessageMode mode, String username) {
-        switch (mode) {
-            case SEND -> {
-                outWriter.println(constructMessage("HELLO", new String[]{username}));
+    private void eval(String command, ArrayList<String> args) throws IndexOutOfBoundsException {
+        switch (command) {
+            case "HELLO" -> {
+                // HELLO;<VERSION>;<MAX_SEMESTER>;<USERNAME>
+                // Die erste Nachricht der Verbindung und wird nach Herstellung der SocketVerbindung von dem Host an den
+                // Client gesendet
+                int peerVersion = Integer.parseInt(args.get(0));
+                int peerSemester = Integer.parseInt(args.get(1));
+                String peerUsername = args.get(2);
+                HELLO(RECEIVE, peerVersion, peerSemester, peerUsername);
             }
-            case RECEIVE -> {
-                peerUsername = username;
+            case "HELLO_ACK" -> {
+                // HELLO_ACK;<VERSION>;<SEMESTER>;<USERNAME>
+                // Diese Nachricht wird als Antwort auf eine HELLO-Nachricht gesendet.
+                int peerVersion = Integer.parseInt(args.get(0));
+                int peerSemester = Integer.parseInt(args.get(1));
+                String peerUsername = args.get(2);
+                HELLO_ACK(RECEIVE, peerVersion, peerSemester, peerUsername);
             }
-        }
-    }
-
-    private void VERSION_SEND(MessageMode mode, int protocolVersion) {
-        switch (mode) {
-            case SEND -> {
-                outWriter.println(constructMessage("VERSION_SEND", new String[]{String.valueOf(implementedProtocolVersion)}));
+            case "START" -> {
+                // START
+                START(RECEIVE);
             }
-            case RECEIVE -> {
-                // if this is the first time receiving this message, calc protocol version and send ack
-                // if protocol version was already negotiated before, return without action
-                // TODO: decide whether to allow re-negotiation
-                if (protocolVersionNegotiated) {
-                    log_stderr("Peer trying to re-negotiate protocol version, no action taken");
-                    return;
-                }
-                this.protocolVersion = Math.min(implementedProtocolVersion, protocolVersion);
-                VERSION_ACK(SEND, this.protocolVersion);
+            case "START_ACK" -> {
+                // START_ACK
+                START_ACK(RECEIVE);
             }
-        }
-    }
-
-    private void VERSION_ACK(MessageMode mode, int protocolVersion) {
-        switch (mode) {
-            case SEND -> {
-                outWriter.println(constructMessage("VERSION_ACK", new String[]{String.valueOf(protocolVersion)}));
+            case "READY_PING" -> {
+                // READY_PING;<SHIPS_PLACED>
+                int peerShipsPlaced = Integer.parseInt(args.get(0));
+                READY_PING(RECEIVE, peerShipsPlaced);
             }
-            case RECEIVE -> {
-                if (!protocolVersionNegotiated && this.protocolVersion == protocolVersion) {
-                    protocolVersionNegotiated = true;
-                } else if (protocolVersionNegotiated) {
-                    log_stderr("Peer trying to re-negotiate protocol version, no action taken");
+            case "READY_CHK" -> {
+                // READY_CHK
+                READY_CHK(RECEIVE);
+            }
+            case "BEGIN" -> {
+                // BEGIN;<WHO>
+                String who = args.get(0);
+                BEGIN(RECEIVE, who);
+            }
+            case "BEGIN_ACK" -> {
+                // BEGIN_ACK;<WHO>
+                String who = args.get(0);
+                BEGIN_ACK(RECEIVE, who);
+            }
+            case "FIRE" -> {
+                // FIRE;<ROW>;<COLUMN>
+                int row = Integer.parseInt(args.get(0));
+                int col = Integer.parseInt(args.get(1));
+                FIRE(RECEIVE, row, col);
+            }
+            case "FIRE_ACK" -> {
+                // FIRE_ACK;<TYPE>[;<GAME_OVER>]
+                String type = args.get(0);
+                if (args.size() >= 2) {
+                    boolean gameOver = Boolean.parseBoolean(args.get(1));
+                    FIRE_ACK(RECEIVE, type, gameOver);
                 } else {
-                    log_stderr("Failed to negotiate protocol version");
+                    FIRE_ACK(RECEIVE, type);
                 }
             }
+            case "END" -> {
+                // END;<WINNER>
+            }
+            case "END_ACK" -> {
+                // END_ACK
+            }
+            case "BYE" -> {
+                // BYE;[<CODE>;<REASON>]
+            }
+            case "ERR" -> {
+                // ERR;<CODE>;<REASON>
+            }
+
         }
+
+
     }
 
-    private void SEMESTER_SEND(MessageMode mode, int semester) {
+    private void HELLO(MessageMode mode, int VERSION, int MAX_SEMESTER, String USERNAME) {
         switch (mode) {
             case SEND -> {
-                outWriter.println(constructMessage("SEMESTER_SEND", new String[]{String.valueOf(semester)}));
+                sendMessage("HELLO", String.valueOf(VERSION), String.valueOf(MAX_SEMESTER), USERNAME);
             }
             case RECEIVE -> {
-                // if this is the first time receiving this message, calc common semester and send ack
-                // if semester was already negotiated before, return without action
-                // TODO: decide whether to allow re-negotiation
-                if (semesterNegotiated) {
-                    log_stderr("Peer trying to re-negotiate semester, no action taken");
-                    return;
+                if (protocolVersionNegotiated || semesterNegotiated) {
+                    log_stderr("HELLO: attempted re-negotiation of protocol or semester");
+                    System.exit(1);
                 }
-                this.semester = Math.min(this.semester, semester);
-                SEMESTER_ACK(SEND, this.semester);
+                protocolVersion = Math.min(implementedProtocolVersion, VERSION);
+                protocolVersionNegotiated = true;
+                semester = Math.min(semester, MAX_SEMESTER);
+                semesterNegotiated = true;
+                peerUsername = USERNAME;
+                log_debug("HELLO: protocol version: " + protocolVersion + ", semester: " + semester + ", peerUsername: " + peerUsername);
+                HELLO_ACK(SEND, protocolVersion, semester, username);
             }
         }
     }
 
-    private void SEMESTER_ACK(MessageMode mode, int semester) {
+    private void HELLO_ACK(MessageMode mode, int VERSION, int SEMESTER, String USERNAME) {
         switch (mode) {
             case SEND -> {
-                outWriter.println(constructMessage("SEMESTER_ACK", new String[]{String.valueOf(semester)}));
+                sendMessage("HELLO_ACK", String.valueOf(VERSION), String.valueOf(SEMESTER), USERNAME);
             }
             case RECEIVE -> {
-                if (!semesterNegotiated && this.semester == semester) {
-                    semesterNegotiated = true;
-                } else if (semesterNegotiated) {
-                    log_stderr("Peer trying to re-negotiate semester, no action taken");
-                } else {
-                    log_stderr("Failed to negotiate semester version");
+                if (VERSION > implementedProtocolVersion) {
+                    log_stderr("HELLO_ACK: VERSION > implementedProtocolVersion (" + VERSION + ">" + implementedProtocolVersion + ")");
+                    System.exit(1);
                 }
-            }
-        }
-    }
+                if (SEMESTER > semester) {
+                    log_stderr("HELLO_ACK: SEMESTER > this.semester (" + SEMESTER + ">" + semester + ")");
+                    System.exit(1);
+                }
+                if (protocolVersionNegotiated || semesterNegotiated) {
+                    log_stderr("HELLO_ACK: attempted re-negotiation of protocol or semester");
+                    System.exit(1);
+                }
 
-    private void READY_CHECK(MessageMode mode) {
-        switch (mode) {
-            case SEND -> {
-            }
-            case RECEIVE -> {
-            }
-        }
-    }
-
-    private void READY_RESPONSE(MessageMode mode) {
-        switch (mode) {
-            case SEND -> {
-            }
-            case RECEIVE -> {
+                protocolVersion = VERSION;
+                protocolVersionNegotiated = true;
+                semester = SEMESTER;
+                semesterNegotiated = true;
+                peerUsername = USERNAME;
+                log_debug("HELLO_ACK: protocol version: " + protocolVersion + ", semester: " + semester + ", peerUsername: " + peerUsername);
+                START(SEND);
             }
         }
     }
 
     private void START(MessageMode mode) {
+        // TODO set a boolean here?
         switch (mode) {
             case SEND -> {
+                sendMessage("START");
             }
             case RECEIVE -> {
+                START_ACK(SEND);
             }
         }
     }
 
     private void START_ACK(MessageMode mode) {
+        // TODO set a boolean here?
+        switch (mode) {
+            case SEND -> {
+                sendMessage("START_ACK");
+            }
+            case RECEIVE -> {
+            }
+        }
+    }
+
+    private void READY_PING(MessageMode mode, int SHIPS_PLACED) {
+        switch (mode) {
+            case SEND -> {
+                // TODO expose this to logic
+                sendMessage("READY_PING", String.valueOf(SHIPS_PLACED));
+            }
+            case RECEIVE -> {
+                // TODO forward SHIPS_PLACED to logic
+                // TODO get info from logic on how many ships we have placed to be able to respond
+                int playerShipsPlaced = 0;      // dummy
+                // TODO get info what maxPlacedShips for current semester is (from logic?)
+                int maxPlacedShips = 5;         // dummy
+
+                // TODO does this not cause an infinite loop / spam until all ships are placed?
+                if (playerShipsPlaced == maxPlacedShips) {
+                    READY_CHK(SEND);
+                } else {
+                    // FIXME maybe the api gets changed?
+                    // if we received READY_PING, wait before sending response to avoid spam
+                    new java.util.Timer().schedule(
+                            new java.util.TimerTask() {
+                                @Override
+                                public void run() {
+                                    READY_PING(SEND, playerShipsPlaced);
+                                }
+                            }, 500
+                    );
+                }
+            }
+        }
+    }
+
+    private void READY_CHK(MessageMode mode) {
+        switch (mode) {
+            case SEND -> {
+                // TODO expose this to logic
+                sendMessage("READY_CHK");
+            }
+            case RECEIVE -> {
+                // TODO forward this info to logic
+            }
+        }
+    }
+
+    private void BEGIN(MessageMode mode, String WHO) {
+        // TODO set a boolean here?
+        switch (mode) {
+            case SEND -> {
+                // TODO expose this to logic
+                // TODO decide who rolls the random value
+                sendMessage("BEGIN", WHO);
+            }
+            case RECEIVE -> {
+                // TODO forward this info to logic
+                String whoInverse = WHO.equalsIgnoreCase("host") ? "client" : "host";
+                BEGIN_ACK(SEND, whoInverse);
+            }
+        }
+    }
+
+    private void BEGIN_ACK(MessageMode mode, String WHO) {
+        switch (mode) {
+            case SEND -> {
+                sendMessage("BEGIN_ACK", WHO);
+            }
+            case RECEIVE -> {
+                // TODO do we need to do anything here?
+            }
+        }
+    }
+
+    private void FIRE(MessageMode mode, int ROW, int COL) {
+        switch (mode) {
+            case SEND -> {
+                // TODO expose this to logic
+                sendMessage("FIRE", String.valueOf(ROW), String.valueOf(COL));
+            }
+            case RECEIVE -> {
+                // TODO forward this info to logic
+            }
+        }
+    }
+
+    private void FIRE_ACK(MessageMode mode, String TYPE, boolean GAME_OVER) {
+        switch (mode) {
+            case SEND -> {
+                // TODO expose this to logic
+                sendMessage("FIRE_ACK", TYPE, String.valueOf(GAME_OVER));
+            }
+            case RECEIVE -> {
+                // TODO forward this info to logic
+                // TODO special handling for GAME_OVER?
+            }
+        }
+    }
+
+    private void FIRE_ACK(MessageMode mode, String TYPE) {
+        // TODO is this the correct handling of GAME_OVER?
+        FIRE_ACK(mode, TYPE, false);
+    }
+
+
+    private void ERR(MessageMode mode) {
         switch (mode) {
             case SEND -> {
             }
@@ -238,48 +366,27 @@ public final class Contact {
         }
     }
 
-    private void FIRE(MessageMode mode) {
-        switch (mode) {
-            case SEND -> {
-            }
-            case RECEIVE -> {
-            }
-        }
-    }
-
-    private void FIRE_ACK(MessageMode mode) {
-        switch (mode) {
-            case SEND -> {
-            }
-            case RECEIVE -> {
-            }
-        }
-    }
-
-    private void ERROR(MessageMode mode) {
-        switch (mode) {
-            case SEND -> {
-            }
-            case RECEIVE -> {
-            }
-        }
-    }
-
-
-    public void sendMessage(String msg) {
+    private void sendRawMessage(String msg) {
         outWriter.println(msg);
     }
 
-    public void setSocket(Socket clientSocket) throws IOException {
+    public void sendMessage(String command, String... args) {
+        String msg = constructMessage(command, args);
+        log_debug("sending: '" + msg + "'");
+        outWriter.println(msg);
+    }
+
+    public void setSocket(@NotNull Socket clientSocket) throws IOException {
+        if (socket != null) {
+            log_stderr("Attempting to overwrite socket");
+            System.exit(1);
+        }
+
         this.socket = clientSocket;
 
         this.outWriter = new PrintWriter(this.socket.getOutputStream(), true);
         this.inReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
 
-        new Thread(this::receiveLoop).start();
-        HELLO(SEND, this.username);
-        VERSION_SEND(SEND, implementedProtocolVersion);
-        SEMESTER_SEND(SEND, semester);
-
+        init_communication();
     }
 }
