3379922b845189008b6c11e0b4358d022a8df65b
Jannik Rosendahl <jannik.rosendahl@stud.hs-hannover.de>
2022-11-01 19:57:06 +0100

    add logging timestamps

--- src/network/internal/Util.java
+++ src/network/internal/Util.java
@@ -1,10 +1,15 @@
 package network.internal;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public final class Util {
     private Util() {
     }
 
     public static final boolean debug = true;
+
+    public static int implementedProtocolVersion = 1;
 
     public static final String defaultAddress = "localhost";
     public static final int defaultPort = 42069;
@@ -25,21 +30,27 @@ public final class Util {
         return builder.toString();
     }
 
-    private static final String logPrefix = "Network: ";
+    // log
+    private static final DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss :: ");
+
+    private static String getDateTimePrefix() {
+        return dtFormatter.format(LocalDateTime.now());
+    }
+
+    private static final String logPrefix = "Network :: ";
 
     public static void log_stdio(String msg) {
-        System.out.println(logPrefix + msg);
+        System.out.println(getDateTimePrefix() + logPrefix + msg);
     }
 
     public static void log_debug(String msg) {
         if (!debug)
             return;
-        log_stdio("debug: " + msg);
+        log_stdio("debug :: " + msg);
     }
 
     public static void log_stderr(String msg) {
-        System.err.println(logPrefix + msg);
+        System.err.println(getDateTimePrefix() + logPrefix + msg);
     }
 
-    public static int implementedProtocolVersion = 1;
 }
