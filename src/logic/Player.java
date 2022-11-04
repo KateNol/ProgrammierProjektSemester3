74f4f9bc33ed4b2452ef3ee25b4f4a26848aca94
Jannik Rosendahl <jannik.rosendahl@me.com>
2022-11-04 11:54:04 +0100

    add comments to Player

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -4,18 +4,57 @@ import java.util.Observable;
 import java.util.Random;
 
 public abstract class Player extends Observable {
+    /**
+     * returns true when both players are connected
+     * implemented by NetworkPlayer
+     * @return
+     */
     public abstract boolean getIsConnected();
 
+    /**
+     * returns the highest semester both players can play in
+     * a call to this is only valid if getIsConnected() returns true
+     * @return
+     */
     public abstract int getCommonSemester();
 
+    /**
+     * gets our username
+     * implemented by NetworkPlayer
+     * @return
+     */
     public abstract String getUsername();
 
+    /**
+     * requests a coordinate from the player
+     * has to be implemented by GUIPlayer/AIPlayer/ConsolePlayer
+     * @return the point where your player wants to shoot
+     */
     public abstract Coordinate getShot();
 
+    /**
+     * asks the player to send our shot to the enemy player
+     * implemented by NetworkPlayer
+     * @param coordinate
+     */
     public abstract void sendShot(Coordinate coordinate);
 
-    public abstract void sendShotResponse(ShotResult shotResult);
+    /**
+     * call when we have evaluated a shot from the enemy (received by notify())
+     * sends the result to the enemy shot back to the enemy
+     * implemented by NetworkPlayer
+     * @param shotResult
+     */
+    public void sendShotResponse(ShotResult shotResult) {
+        // Ewpojwpoegj
+    }
 
+    /**
+     * this method will be called by Logic when a shot was received via notify()
+     * may be overwritten by GUIPlayer/AIPlayer/ConsolePlayer
+     * @param shot
+     * @return
+     */
     public ShotResult receiveShot(Coordinate shot) {
         // TODO look up actual result in map
         Random random = new Random();
