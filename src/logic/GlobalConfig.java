d0a2374dbae64ce066bb9a2f24b24b3b3ce3a1cd
Philip <philip.ohizu@stud.hs-hannover.de>
2022-11-08 20:31:34 +0100

    just some global config update

--- src/logic/GlobalConfig.java
+++ src/logic/GlobalConfig.java
@@ -3,12 +3,30 @@ package logic;
 /**
  * return values are just for debug
  */
+
+
 public class GlobalConfig {
-    public int[] getShipSizes(int commonSemester) {
-        return new int[]{2,2,2,2,4,6};
+    private final int ships[][] = {{2, 2, 2, 2, 4, 6}, {2, 2, 2, 2, 2}, {2, 2, 2, 2, 4, 6}, {2, 2, 2, 2, 4, 6}, {2, 2, 2, 3, 3, 6}, {2, 1, 1, 1, 6}};
+    private int  mapSize = 14;
+
+
+    public GlobalConfig() {
     }
 
+
     public int getMapSize(int commonSemester) {
-        return 14;
+        mapSize  += commonSemester;
+        return mapSize;
     }
+
+    public int[] getShipSizes(int commonSemester) {
+        int neuShip[] = new int[ships[commonSemester - 1].length];
+        for (int i = 0; i < ships[commonSemester - 1].length; i++) {
+            neuShip[i] = ships[commonSemester - 1][i];
+        }
+        return neuShip;
+
+    }
+
+
 }
