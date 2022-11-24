d749686fbdad0b00f941ca4cd030d78415998d46
Jannik Rosendahl <jannik.rosendahl@me.com>
2022-11-24 16:19:58 +0100

    add 2 new util functions

--- src/logic/Util.java
+++ src/logic/Util.java
@@ -2,6 +2,8 @@ package logic;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Util {
 
@@ -29,4 +31,37 @@ public class Util {
         System.err.println(getDateTimePrefix() + logPrefix + msg);
     }
 
+    /**
+     * returns list of all (max 8) Coordinates surrounding c
+     * @param c target Coordinate
+     * @param max max allowed value, set to mapsize -1
+     * @return
+     */
+    public static List<Coordinate> getSurroundingCoordinates(Coordinate c, int mapSize) {
+        List<Coordinate> result = new ArrayList<>();
+
+        int []arr = { -1, 0, 1 };
+
+        for (int deltax : arr) {
+            for (int deltay : arr) {
+                if (deltax == 0 && deltay == 0)
+                    continue;
+
+                Coordinate current = new Coordinate(c.row() + deltax, c.col() + deltay);
+
+                if (current.row() < 0 || current.row() >= mapSize)
+                    continue;
+                if (current.col() < 0 || current.col() >= mapSize)
+                    continue;
+
+                result.add(current);
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean getCoordinateInBounds(Coordinate c, int mapSize) {
+        return c.row() > 0 && c.col() > 0 && c.row() < mapSize-1 && c.col() < mapSize-1;
+    }
 }
