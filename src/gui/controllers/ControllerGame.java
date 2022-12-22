f00dabeeee2baa42b4bf49f5011db62ca629b818
Katharina Nolte <katharina.nolte@stud-hs-hannover.de>
2022-12-22 12:23:58 +0100

    change images in Backround

--- src/gui/controllers/ControllerGame.java
+++ src/gui/controllers/ControllerGame.java
@@ -68,7 +68,7 @@ public class ControllerGame implements Initializable {
 
         //background music & picture
         AudioPlayer.playMusic(Audio.BattleMusic2);
-        background.setBackground(Settings.setBackgroundImage("file:src/gui/img/Lernnische_HsH.png"));
+        background.setBackground(Settings.setBackgroundImage("file:src/gui/img/HörsaalH.0.01.png"));
 
         //Set Boards
         guiPlayer.getGuiBoard().getInitializedBoard(myBoard);
